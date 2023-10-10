37b553f108430afddf16b3bbb5664b7492b016ac
kevinhouguet <k.houguet@gmail.com>
2023-10-10 16:13:17 +0200

    :sparkles: features(client): add header and menu component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,20 @@
+import Header from "./components/header"
+import { useEffect } from "react"
+
 export default function App() {
+
+  const headerProps = {
+    title: 'It\'s a CRApp!'
+  }
+
+  useEffect(() => {
+    console.log('App component mounted');
+    
+  });
 
   return (
     <>
-      <h1>Title</h1>
+      <Header {...headerProps} />
     </>
   )
 }
---
+++ front/src/components/header/index.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Menu from '../menu';
+
+export interface IHeaderProps {
+  title: string;
+}
+
+export default class Header extends React.Component<IHeaderProps, IHeaderProps> {
+  constructor(props: IHeaderProps) {
+    super(props)
+    this.state = {
+      title: props.title || 'Put a title here',
+    }
+  }
+
+  render() {
+
+    return (
+      <>
+        <h1>{this.state.title}</h1>
+        <Menu />
+      </>
+    )
+  }
+}
