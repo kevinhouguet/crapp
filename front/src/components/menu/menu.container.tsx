a7a1132da619f571ba0d27dc2f80f22ef65b3e1f
kevinhouguet <k.houguet@gmail.com>
2023-10-16 11:34:20 +0200

    :hammer: refactor(menu): more lisible

--- front/src/components/menu/menu.container.tsx
+++ front/src/components/menu/menu.container.tsx
@@ -2,73 +2,75 @@ import React, { Component } from "react";
 import Menu, { IMenuProps } from "./menu.view";
 import User from "../../models/user/user";
 import Member from "../../models/user/user.member";
+import { MenuItem } from "./menuItem";
 
-export default class MenuContainer extends Component {
+export default class MenuContainer extends Component <Record<string, never>, IMenuProps>{
 
-  constructor(props: IMenuProps) {
+  public menuForGuest: MenuItem[] = [{
+      displayName: 'Login',
+      URL: '/login',
+      onClick: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => this.onLogin(e)
+    }]
+
+  public menuForMember: MenuItem[] = [{
+      displayName: 'Profile',
+      URL: '/profile',
+      onClick: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => this.onProfil(e)
+    }, {
+      displayName: 'Logout',
+      URL: '/logout',
+      onClick: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => this.onLogout(e)
+    }];
+
+  constructor(props = {}) {
     super(props);
     this.state = {
-      menuItems: [{
-        displayName: 'Login',
-        URL: '/login',
-        onClick: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => this.onLogin(e)
-      }],
-    };
+      menuItems: this.menuForGuest
+    } as IMenuProps;
   }
 
-  isUserLoggedIn() {
+  isUserLoggedIn(): boolean{
     return User.isLoggedIn();
   }
 
-  onLogout(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
-    event.preventDefault();
-    console.log('Logout clicked');
-    Member.logout();
+  setMenuForMember(): void {
     this.setState({
-      menuItems: [{
-        displayName: 'Login',
-        URL: '/login',
-        onClick: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => this.onLogin(e)
-      }]
+      menuItems: this.menuForMember
     });
   }
 
-  onProfil(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
-    event.preventDefault();
-    console.log('Profile clicked');
+  setMenuForGuest(): void{
+    this.setState({
+      menuItems: this.menuForGuest
+    });
   }
 
-  onLogin = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+  onLogout(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void {
+    event.preventDefault();
+    Member.logout();
+    
+    this.setMenuForGuest();
+  }
+
+  onProfil(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void {
+    event.preventDefault();
+  }
+
+  onLogin(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void {
     event.preventDefault();
     if(this.isUserLoggedIn()) {
-      console.log('User is already logged in')
       return;
     }
     const user = new Member(1,'John Doe', 'jd@gmail.com');
     user.login();
     
-    this.setState({
-      menuItems: [{
-        displayName: 'Profile',
-        URL: '/profile',
-        onClick: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => this.onProfil(e)
-      },{
-        displayName: 'Logout',
-        URL: '/logout',
-        onClick: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => this.onLogout(e)
-      }],
-    });
-
-    console.log('Login clicked');
+    this.setMenuForMember();
   }
 
   render() {
-    // console.log(this.state.menuItems);
     return (
       <>
-        <Menu
-          menuItems={this.state.menuItems}
-        />
+        <Menu menuItems={this.state.menuItems} />
       </>
     );
   }
